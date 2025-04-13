f9b9c01c271f8eb005f1f853670b39a0621607bb
JAYARAMAN AJITH KUMAR <ajithramykumar@gmail.com>
2025-04-13 18:57:57 +0200

    Add files via upload

---
+++ #python calculator.py
@@ -0,0 +1,9 @@
+#python calculator 
+while True:
+    operator =input("choos the operator:")
+    a =float(input("Enter the first vlaue:"))
+    b =float(input("Enter the second value:"))
+ if operator.lower()== "q":
+    break
+
+ 
