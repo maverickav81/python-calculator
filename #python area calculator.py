e44b4f544fc7cfc0959223fe2ec6e58660558a1f
JAYARAMAN AJITH KUMAR <ajithramykumar@gmail.com>
2025-04-17 16:35:49 +0200

    Update #python area calculator.py

--- #python area calculator.py
+++ #python area calculator.py
@@ -2,8 +2,20 @@
 import math
 while True:
     print("Which shape? \n1) Triangle\n2) Rectangle\n3) Square\n4) Circle\n5) Quit")
-    shape =int(input("Choose the shape(1 to 5): "))
-    #Rectangle
+    shape_input =input("Choose the shape(1 to 5): ")
+    if not shape_input.strip():
+        print("Please enter valid value: ")
+        continue
+    if not shape_input.isdigit():
+        print("Please enter the value in digit")
+        continue
+      
+     shape =int(shape_input)
+        
+    if shape <1 or shape >5 :
+        print("The value should be between 1 to 5")
+        continue
+ #Rectangle
     if shape == 1:
         height =float(input("Enter the Height: "))
         base =float(input("Enter the base: "))
